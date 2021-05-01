4e7c9cae75947c7608ef18b7979b1ba82667ca74
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-05-01 13:44:29 -0400

    viewImageScreen

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
